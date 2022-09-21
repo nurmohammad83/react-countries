d21a32098746182ef2cb4beeaa63c403673c2a80
nurmohammad83 <pesfootball83@gmail.com>
2022-09-21 09:19:43 +0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
+// import { useEffect, useState } from "react";
+import Header from './component/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
+// function LoadCountries(){
+//   const [Countries, setCountries] = useState([])
+//   useEffect(()=>{
+//     const url=`https://restcountries.com/v3.1/all
+//     `
+//     fetch(url)
+//     .then(res=>res.json())
+//     .then(data=>setCountries(data))
+//   })
+//   return(
+//     <div>
+//       <h1>Visiting Every Counties World</h1>
+//       {
+//         Countries.map(counties=><User name={counties.name.common} population={counties.population} img={counties.flags.png}></User>)
+//       }
+//     </div>
+//   )
+// }
+
+// function User(props){
+//   return(
+//     <div className='user'>
+//     <h1>{props.name}</h1>
+//     <p>{props.population}</p>
+//     <img src={props.img} alt="" />
+//     </div>
+    
+//   )
+// }
 
 export default App;
+
+
---
+++ src/component/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <li>Home</li>
+                <li>Service</li>
+                <li>Contract</li>
+                <li>Blog</li>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
